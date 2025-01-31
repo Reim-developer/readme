564b87a3d3607a3ea3aa5a5e2c9217ee0961bdd7
Reim-developer <contact.kaxtr@gmail.com>
2025-01-31 21:43:48 +0700

    Added new global Ulti class * ImageSet

---
+++ src/main/java/readme/app/ulti/ImageSet.java
@@ -0,0 +1,17 @@
+package readme.app.ulti;
+
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.widgets.Display;
+
+public class ImageSet {
+    public Image imageButton(Display display, String imagePath, int width, int height) {
+        Image originalImg = new Image(display,
+                getClass().getResourceAsStream(imagePath)
+        );
+        ImageData imageData = originalImg.getImageData();
+        ImageData scaledImgData = imageData.scaledTo(width, height);
+
+        return new Image(display, scaledImgData);
+    }
+}
