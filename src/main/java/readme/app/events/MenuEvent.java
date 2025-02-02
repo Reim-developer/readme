dec2bb1d309a3d5109f5a77eb7731e875f2ce3e2
Reim-developer <contact.kaxtr@gmail.com>
2025-02-02 20:19:56 +0700

    Added comment suggest

--- src/main/java/readme/app/events/MenuEvent.java
+++ src/main/java/readme/app/events/MenuEvent.java
@@ -18,12 +18,21 @@ public class MenuEvent {
         this.taskbar.setTaskbarUI(application.shell, application.display);
     }
 
+    /**
+     * When user click to Menu Button,
+     * we show a Taskbar Menu.
+     * @param button Button to add SWT Selection event listener
+     */
+
     public void openMenu(@NotNull Button button) {
         button.addListener(SWT.Selection,  event -> {
             this.isVisible = !isVisible;
 
             this.taskbar.showTaskbar(isVisible);
             this.application.shell.layout();
+
+            if(isVisible) { button.setToolTipText("Hide Main Menu"); return; }
+            button.setToolTipText("Show Main Menu");
         });
     }
 }
