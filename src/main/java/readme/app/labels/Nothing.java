442dbd39537c8a08b3ca37427209684334b4d114
Reim-developer <contact.kaxtr@gmail.com>
2025-02-03 22:58:02 +0700

    Fixed UI for * Nothing label

--- src/main/java/readme/app/labels/Nothing.java
+++ src/main/java/readme/app/labels/Nothing.java
@@ -4,20 +4,37 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
+import org.jetbrains.annotations.NotNull;
+import readme.app.Application;
+import readme.app.ulti.Memory;
 
 public class Nothing {
-    public void setNothingToShow(Composite composite, Display display) {
-        Label label = new Label(composite, SWT.NONE);
+    private final Label label;
+    private final Memory memory;
+
+    public Nothing(@NotNull Application application) {
+        this.label = new Label(application.shell, SWT.NONE);
+        this.memory = new Memory();
+    }
+
+    public void setNothingLabel(Display display) {
+        Font font = new Font(display, "Consolas", 12, SWT.NONE);
 
         label.setText("There is nothing to show.");
-        label.setFont(new Font(display, "Consolas", 12, SWT.NONE));
+        label.setFont(font);
 
         FormData formData = new FormData();
         formData.left = new FormAttachment(50, -100);
         formData.top = new FormAttachment(50, 0);
         label.setLayoutData(formData);
+
+        label.setVisible(false);
+        this.memory.dispose(label, font);
+    }
+
+    public void showNothingLabel(boolean visible) {
+        this.label.setVisible(visible);
     }
 }
