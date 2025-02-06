7ff39e5481adaec3e1801a635b2e2901de4d5327
Reim-developer <contact.kaxtr@gmail.com>
2025-02-06 22:22:29 +0700

    Added AppContext and init this to Application.java, main class

--- src/main/java/readme/app/Application.java
+++ src/main/java/readme/app/Application.java
@@ -3,27 +3,16 @@ package readme.app;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.FormLayout;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
 import readme.app.buttons.MainMenu;
-import readme.app.labels.Nothing;
 import readme.app.props.Size;
-import readme.app.ulti.Memory;
-
 
 public class Application {
-    public final Display display;
-    public final Shell shell;
     private final MainMenu mainMenu;
-    private final Nothing nothingLabel;
-    private final Memory memory;
+    private final AppContext appContext;
 
     public Application() {
-        this.display = new Display();
-        this.shell = new Shell(this.display);
-        this.mainMenu = new MainMenu(this);
-        this.nothingLabel = new Nothing(this);
-        this.memory = new Memory();
+        this.appContext = new AppContext();
+        this.mainMenu = new MainMenu(this.appContext);
     }
 
     public static void main(String[] args) {
@@ -33,23 +22,22 @@ public class Application {
     }
 
     private void init() {
-        Color color = new Color(this.display, 47, 49, 54);
+        Color color = new Color(this.appContext.display, 47, 49, 54);
 
-        this.shell.setText("readme!");
-        this.shell.setSize(new Point(Size.APP_WIDTH, Size.APP_HEIGHT));
-        this.shell.setLayout(new FormLayout());
+        this.appContext.shell.setText("readme!");
+        this.appContext.shell.setSize(new Point(Size.APP_WIDTH, Size.APP_HEIGHT));
+        this.appContext.shell.setLayout(new FormLayout());
+        this.mainMenu.setMainMenuButton(this.appContext.shell, appContext.display);
+        this.appContext.shell.setBackground(color);
 
-        this.mainMenu.setMainMenuButton(shell, display);
-        this.nothingLabel.setNothingLabel(display);
-        this.nothingLabel.showNothingLabel(true);
+        this.appContext.getNothingLabel().label.setVisible(true);
 
-        this.shell.setBackground(color);
-        this.shell.open();
-        this.memory.dispose(shell, color);
+        this.appContext.shell.open();
 
-        while (!this.shell.isDisposed()) {
-            if(!this.display.readAndDispatch()) this.display.sleep();
+        while (!this.appContext.shell.isDisposed()) {
+            if(!this.appContext.display.readAndDispatch()) this.appContext.display.sleep();
         }
-        this.shell.dispose();
+        color.dispose();
+        this.appContext.shell.dispose();
     }
 }
