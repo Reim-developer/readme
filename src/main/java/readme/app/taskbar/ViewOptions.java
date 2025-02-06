6dd1811044f9cc5485688970c578f1ded65a33da
Reim-developer <contact.kaxtr@gmail.com>
2025-02-06 22:25:42 +0700

    Fixed event and remove Application class on constructor parameter and replace this with AppContext

--- src/main/java/readme/app/taskbar/ViewOptions.java
+++ src/main/java/readme/app/taskbar/ViewOptions.java
@@ -8,7 +8,8 @@ import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
 import org.jetbrains.annotations.NotNull;
-import readme.app.Application;
+import readme.app.AppContext;
+import readme.app.events.WordWrap;
 import readme.app.ulti.Memory;
 
 public class ViewOptions {
@@ -18,17 +19,18 @@ public class ViewOptions {
     private final Color color;
     private final Memory memory;
     private boolean isMouseOverTable;
+    private final WordWrap wordWrap;
 
-    public ViewOptions(@NotNull Application application) {
-        this.table = new Table(application.shell, SWT.FULL_SELECTION);
+    public ViewOptions(@NotNull AppContext appContext) {
+        this.table = new Table(appContext.shell, SWT.FULL_SELECTION);
         this.tableColumn = new TableColumn(this.table, SWT.CENTER);
         this.viewOptions = new String[]{"Enable word wrap", "Zoom in", "Zoom out"};
-        this.color = new Color(application.display, 47, 49, 54);
+        this.color = new Color(appContext.display, 47, 49, 54);
+        this.wordWrap = new WordWrap(appContext);
         this.memory = new Memory();
 
         setViewOptions();
     }
-
 
     private void setViewOptions() {
         this.table.setBackground(this.color);
@@ -48,13 +50,18 @@ public class ViewOptions {
         this.table.addListener(SWT.MouseEnter, event -> this.isMouseOverTable = true);
         this.table.addListener(SWT.MouseExit, event -> this.isMouseOverTable = false);
 
+        this.wordWrap.setWordWrap(this.table);
+
         this.table.setVisible(false);
         this.memory.dispose(this.table, this.color);
+
     }
 
     public void showViewOptions(boolean visible) {
         this.table.setVisible(visible);
     }
 
-    public boolean isMouseOverTable() { return isMouseOverTable; }
+    public boolean isMouseOverTable() {
+        return isMouseOverTable;
+    }
 }
